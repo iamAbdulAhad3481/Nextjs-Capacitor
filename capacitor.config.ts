c42c0cb161ace157d36a5a0499bfbf2fd6952c64
Abdul Ahad <abdulahad@visualr.com.au>
2023-02-14 09:49:47 +0500

    initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.example.app",
+  appName: "capacitor-nextjs-sample",
+  webDir: "out",
+  bundledWebRuntime: false,
+};
+
+export default config;
